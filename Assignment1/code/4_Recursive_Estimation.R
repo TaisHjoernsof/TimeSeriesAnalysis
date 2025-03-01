de8c7a2cf4420f0a5f15670cb539ac280b3e7b6c
Tais <s193900@dtu.dk>
2025-03-01 12:20:45 +0100

    Calculating for 4.3

--- Assignment1/code/4_Recursive_Estimation.R
+++ Assignment1/code/4_Recursive_Estimation.R
@@ -10,12 +10,12 @@ X = cbind(1, Dtrain$year)
 p <- ncol(X)
 N <- nrow(X)
 R <- diag(0.1, p)
-theta <- c(0,0)#rep(0, p)
+theta <- c(0,0)
 Theta <- matrix(NA, nrow=N, ncol=p)
 
 
 # Iterate through and estimate the parameters
-for(i in 1:3){
+for(i in 1:N){
   (x <- X[i, ])
   # Update
   (R <- R + x %*% t(x))
@@ -23,3 +23,27 @@ for(i in 1:3){
   Theta[i, ] <- theta
 }
 
+# Task 4.3
+Theta_OLS <- matrix(NA, nrow=N, ncol=p)
+for(i in 1:N){
+    m_OLS <- lm(total ~ year, data = Dtrain[1:i,])
+    Theta_OLS[i, ] <- coef(m_OLS)
+}
+
+print(Theta_OLS[N,])
+print(Theta[N,])
+
+# Now checking for better initial values <3
+
+theta = (Theta_OLS[N,])
+
+for(i in 1:N){
+  (x <- X[i, ])
+  # Update
+  (R <- R + x %*% t(x))
+  (theta <- theta + solve(R) %*% x %*% (y[i] - t(x) %*% theta))
+  Theta[i, ] <- theta
+}
+
+print(Theta_OLS[N,])
+print(Theta[N,])
